041dd5fa9839531853bef960841aa1796e30a4dc
Whitigol <whitetigerwolfthe1st@gmail.com>
2024-04-21 23:38:29 -0400

    🎉 init v2

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -1,18 +1,14 @@
-import { MantineProvider, createTheme } from '@mantine/core';
-import { ModalsProvider } from '@mantine/modals';
-import { Notifications } from '@mantine/notifications';
+import ThemeProvider from "./ThemeProvider";
 
 interface Props {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 export default function Providers(props: Props) {
-    return (
-        <MantineProvider defaultColorScheme="dark">
-            <ModalsProvider>
-                <Notifications />
-                {props.children}
-            </ModalsProvider>
-        </MantineProvider>
-    );
+	return (
+		<>
+			<ThemeProvider />
+			<>{props.children}</>
+		</>
+	);
 }
--- src/util/age.ts
+++
@@ -1,6 +0,0 @@
-export default function age() {
-    const birthDate = new Date('2003-07-08');
-    const today = new Date();
-    const age = today.getFullYear() - birthDate.getFullYear();
-    return age;
-}
